1cd604ac9ad9e019c41c0b3ecab35293bb165072
arbach <aarbache@gmail.com>
2020-07-21 18:44:40 +0500

    Added another retry option

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -2,6 +2,7 @@
 const shouldRetry = (error) =>{
     if( error.message.search('out of gas') >= 0 ) return true;   
     if( error.message.search('the tx doesn\'t have the correct nonce') >= 0) return true;
+    if( error.message.search('Invalid JSON RPC response: {}') >= 0) return true;
     return false;    
 };
 
