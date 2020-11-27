f0548886f00ba8a1c7291308792f883ea1be82d3
JL <jerry@onchain.in>
2020-11-27 19:03:47 +0500

    Added account testcases

--- config/papertrail.js
+++ config/papertrail.js
@@ -7,10 +7,12 @@ const winstonPapertrail = new winston.transports.Papertrail({
     port: config.papertrail.port,
     program: config.papertrail.program,
     colorize: true,
+    maximumAttempts: 5,
+    attemptsBeforeDecay: 3,
 })
 
 winstonPapertrail.on('error', function(err) {
-    console.log("Could not connect to Papertrail: ", err.toString());
+    // console.log("Could not connect to Papertrail: ", err.toString());
 });
 
 const logger = new winston.Logger({
--- config/winston.js
+++ config/winston.js
@@ -5,6 +5,10 @@ import config from './config'
 var winstonPapertrail = new winston.transports.Papertrail({
 	host: config.papertrail.host,
     port: config.papertrail.port,
+    program: config.papertrail.program,
+    colorize: true,
+    maximumAttempts: 5,
+    attemptsBeforeDecay: 3,
 })
 
 const logger = new (winston.Logger)({
@@ -18,7 +22,7 @@ const logger = new (winston.Logger)({
 });
 
 winstonPapertrail.on('error', function(err) {
-    console.log("Could not connect to Papertrail: ", err.toString());
+    // console.log("Could not connect to Papertrail: ", err.toString());
 });
 
 export default logger;
